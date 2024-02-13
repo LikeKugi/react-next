5f0734ff6176953dec2d95de1a9b99dede43f798
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2024-02-13 21:08:20 +0300

    feat: add footer layout

--- ps-next/src/layout/Footer/Footer.tsx
+++ ps-next/src/layout/Footer/Footer.tsx
@@ -1,11 +1,17 @@
 import { FC, JSX } from 'react';
 import { IFooterProps } from '@/layout/Footer/Footer.types';
+import { clsx } from 'clsx';
+import styles from './Footer.module.scss'
+import {format} from 'date-fns';
+import Link from 'next/link';
 
-const Footer: FC<IFooterProps> = ({children, ...other}): JSX.Element => {
+const Footer: FC<IFooterProps> = ({children,className , ...other}): JSX.Element => {
   return (
-    <div {...other}>
-      Footer
-    </div>
+    <footer className={clsx(className, styles.Footer)} {...other}>
+      <Link className={styles.Footer__link} href={'/'} target={'_blank'}>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</Link>
+      <Link className={styles.Footer__link} href={'/'} target={'_blank'}>Пользовательское соглашение</Link>
+      <Link className={styles.Footer__link} href={'/'} target={'_blank'}>Политика конфиденциальности</Link>
+    </footer>
   );
 };
 export default Footer;
--- ps-next/src/layout/Layout/Layout.tsx
+++ ps-next/src/layout/Layout/Layout.tsx
@@ -3,19 +3,21 @@ import { ILayoutProps } from '@/layout/Layout/Layout.types';
 import Sidebar from '@/layout/Sidebar/Sidebar';
 import Header from '@/layout/Header/Header';
 import Footer from '@/layout/Footer/Footer';
+import styles from './Layout.module.scss'
+import { Noto_Sans_KR } from "next/font/google";
 
-const Layout: FC<ILayoutProps> = ({children}): JSX.Element => {
+const font = Noto_Sans_KR({ subsets: ["latin"] });
+
+const Layout: FC<ILayoutProps> = ({ children }): JSX.Element => {
   return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        <div>
-          {children}
-        </div>
+    <div className={`${styles.Layout} ${font.className}`}>
+      <Header className={styles.Layout__header}/>
+      <Sidebar className={styles.Layout__sidebar}/>
+      <div className={styles.Layout__body}>
+        {children}
       </div>
-      <Footer />
-    </>
+      <Footer className={styles.Layout__footer}/>
+    </div>
   );
 };
 export default Layout;
